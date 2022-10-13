d58c577ca01e37e427ce762bd4c2bb3ff47e72dd
Luc Ho <hotriluc97@gmail.com>
2022-10-13 17:23:44 +0200

    small adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Navigation from './components/ui/Navigation';
 // import { useAppSelector } from './hooks/app-hooks';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <>
-      <Navigation />
       <AppRoutes />
     </>
   );
